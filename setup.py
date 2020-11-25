7275a928f3c6521671ef401faf87823ce9e7e14f
Ryan Hayward <rhhayward@att.net>
2020-11-25 19:05:32 +0000

    fixed name

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="podcast_generator-rhhayward",
+    name="podcast_generator",
     version="0.0.1",
     author="Ryan Hayward",
     author_email="rhhayward@att.net",
